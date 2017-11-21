2983dd98df3679e29526457241472fd876a2454b
haco1971 <omsameh2010@gmail.com>
2017-11-22 06:28:20 +0700

    Add files via upload

---
+++ play.php
@@ -0,0 +1,3 @@
+<style>body{padding:0px;margin:0px;overflow:hidden;}</style>
+
+<iframe width="720" height="430" scrolling="no" frameborder="0" scrolling="no" allowtransparency="true" src="http://ero-tv.org/uppod/uppod.swf?st=42AEEAG89v5JZja6fcSOW42Qi63GNnZm0nZWwbHsG9n030nLkGcIfwEfpW5ntu0c9BMre&amp;file=rtmp://80.211.154.228:1935/static/<?php echo $_GET['ch']; ?>"></iframe>
